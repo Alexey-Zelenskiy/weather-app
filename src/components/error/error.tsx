ac93c227d52611f0b951e5e339ee02a9ba76ce53
Alexey Zelenskiy <zelenskii1999@mail.ru>
2020-01-29 17:32:53 +0700

    adding error styles

--- src/components/error/error.tsx
+++ src/components/error/error.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 const Error = () => {
-    return <div className="error">Error</div>
+    return <div className="error">Error 404 not found</div>
 };
 
 export default Error;
